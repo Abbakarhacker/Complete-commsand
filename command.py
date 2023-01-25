7b8230064e253d8ac5771df3a4d345682e76e62a
Abbakarhacker <110747527+Abbakarhacker@users.noreply.github.com>
2023-01-25 19:39:10 +0100

    Create command.py

---
+++ command.py
@@ -0,0 +1,179 @@
+import os,time
+import sys
+time.sleep(1)
+os.system("clear")
+
+logo=("""
+\033[1;32m      
+\033[1;31m████████╗███████╗██████╗ ███╗   ███╗██╗   ██╗██╗  ██╗    
+\033[1;33m╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║   ██║╚██╗██╔╝    
+\033[1;37m   ██║   █████╗  ██████╔╝██╔████╔██║██║   ██║ ╚███╔╝     
+\033[1;31m   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║   ██║ ██╔██╗     
+\033[1;35m   ██║   ███████╗██║  ██║██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗    
+\033[1;37m   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝                       
+ \033[1;31m========================================================
+      \033[1;31mPlease subscribe to my YouTube channel 
+ \033[1;31m========================================================                                                                                                                                                                                                                                                                 
+║ \033[1;31m[✓\033[1;32m] \033[1;37mAUTHOR  \033[1;31m:\033[1;32m[+] ABBAKAREO          \033[1;36m                  ║
+║ \033[1;31m[\033[1;32m✓\033[1;31m] \033[1;37mFACEBOOK\033[1;31m:\033[1;32m[+] ABDULLAHI ABUBAKAR         \033[1;36m          ║ 
+║ \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;37m WHATSAPP\033[1;31m:\033[1;32m[+] +2349138749261       \033[1;32m                ║
+║ \033[1;31m[\033[1;32m✓\033[1;31m] \033[1;37mCHANNEL\033[1;31m :\033[1;32m[+] AREWACYBERWARRIORS                 \033[1;32m  ║
+║ \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;37m GITHUB\033[1;31m  :\033[1;32m[+] github.com/Abbakarhacker\033[1;32m             ║
+║ \033[1;31m[\033[1;32m✓\033[1;31m] \033[1;37mVERSION\033[1;31m :\033[1;32m[+] 1.0                \033[1;32m                  ║
+║ \033[1;31m[\033[1;32m✓\033[1;31m] \033[1;37m TOOL \033[1;31m : \033[1;32m[+] TERMUX COMMAND          \033[1;32m             ║
+ ======================================================== 
+""")
+print(logo)
+mtmt=input("\033[1;37m  \n\n\033[1;31m[\033[1;32m01\033[1;31m] \033[1;35mBESIC PACKAGE COMMAND\033[1;32m {BEST}\n\n\033[1;31m[\033[1;32m02\033[1;31m] \033[1;35mFULL COMMAND\n\n WHAT IS YOUR CHOICE : ")
+mtmt=mtmt.replace(" ","")
+if mtmt=="1" or mtmt =="01":
+	print("\n\nINSTALLING START IN 3 SEC...\n\n")
+	os.system("xdg-open https://youtube.com/@arewacyberwarriors")
+	time.sleep(3)
+	os.system("pkg update")
+	os.system("pkg upgrade")
+	os.system("pkg install python -y")
+	os.system("pkg install python2 -y")
+	os.system("pkg install python3")
+	os.system("apt install tmux")
+	os.system("pip install requests")
+	os.system("pip2 install requests")
+	os.system("pip install mechanize")
+	os.system("pip2 install mechanize")
+	os.system("pip install bs4")
+	os.system("pip2 install bs4")
+	os.system("pip install rich")
+	os.system("pkg install ruby -y")
+	os.system("gem install lolcat")
+	os.system("pip install php")
+	os.system("pip2 install php")
+	os.system("pip install lolcat")
+	os.system("pip install futures")
+	os.system("pip2 install futures")
+	os.system("pkg install nano")
+	os.system("clear")
+	print("""\033[1;32m
+ 
+  ____   ___  _   _ _____ 
+ |  _ \ / _ \| \ | | ____|
+ | | | | | | |  \| |  _|  
+ | |_| | |_| | |\  | |___ 
+ |____/ \___/|_| \_|_____|
+                          
+
+
+THANK YOU FOR USING OUR TOOL..
+
+
+
+""")
+elif mtmt=="2" or mtmt=="02":
+	print("SETUP START IN 3 SEC...")
+	time.sleep(3)
+	os.system("pkg update")
+	os.system("pkg upgrade")
+	os.system("pkg install python")
+	os.system("pkg install python2")
+	os.system("pkg install python3")
+	os.system("pkg install wget")
+	os.system("pkg install python-pip")
+	os.system("pkg install fish")
+	os.system("pkg install ruby")
+	os.system("pkg install help")
+	os.system("pkg install dnsutils  ")
+	os.system("pkg install php")
+	os.system("pkg install perl")
+	os.system("pkg install lua")
+	os.system("pkg install parallel")
+	os.system("pkg install nmap")
+	os.system("pkg install bash")
+	os.system("pkg install clang")
+	os.system("pkg install nano")
+	os.system("pkg install w3m")
+	os.system("pkg install hydra")
+	os.system("pkg install figlet")
+	os.system("pkg install cowsay")
+	os.system("pkg install curl")
+	os.system("pkg install tar")
+	os.system("pkg install zip")
+	os.system("pkg install unzip")
+	os.system("pkg install tor")
+	os.system("pkg install net-tools")
+	os.system("pkg install sudo")
+	os.system("pkg install wireshark")
+	os.system("apt install tmux")
+	os.system("pkg install crunch")
+	os.system("pkg install wgetrc")
+	os.system("pkg install wgetrc")
+	os.system("pkg install wcalc")
+	os.system("pkg install openssl")
+	os.system("pkg install openssl-tool")
+	os.system("pkg install bmon")
+	os.system("pkg install vpn")
+	os.system("pkg install unrar")
+	os.system("pkg install toilet")
+	os.system("pkg install proot")
+	os.system("pkg install net-tools")
+	os.system("pkg install vim")
+	os.system("pkg install vim-python")
+	os.system("pkg install ired")
+	os.system("pkg install goaccess")
+	os.system("pkg install golang")
+	os.system("pkg install kibi")
+	os.system("pkg install tsu")
+	os.system("pkg install mtools")
+	os.system("pkg install file")
+	os.system("pkg install vis")
+	os.system("pkg install pass")
+	os.system("pkg install pick")
+	os.system("pkg install chroot")
+	os.system("pkg install macchanger")
+	os.system("pkg install ninja")
+	os.system("pkg install elixir")
+	os.system("pkg install swift")
+	os.system("pkg install xmlstarlet")
+	os.system("pkg install fakeroot")
+	os.system("pkg install texinfo")
+	os.system("pkg install netcat")
+	os.system("pkg install wren")
+	os.system("pkg install gatling")
+	os.system("pkg install cvs")
+	os.system("pkg install ffmpeg")
+	os.system("pkg install screen")
+	os.system("pkg install neofetch")
+	os.system("pkg install mariadb")
+	os.system("pkg install picolisp")
+	os.system("pkg install cmatrix")
+	os.system("pkg install dropbear")
+	os.system("pkg install openssh")
+	os.system("pkg install python-pip")
+	os.system("pip2 install wget")
+	os.system("pip install bs4")
+	os.system("pip2 install bs4")
+	os.system("pip install requests")
+	os.system("pip2 install requests")
+	os.system("pip install mechanize")
+	os.system("pip2 install mechanize")
+	os.system("pip install php")
+	os.system("pip2 install php")
+	os.system("gem install lolcat")
+	os.system("clear")
+	print("""\033[1;32m
+	 
+	 ______   _______  _        _______ 
+(  __  \ (  ___  )( (    /|(  ____ \
+| (  \  )| (   ) ||  \  ( || (    \/
+| |   ) || |   | ||   \ | || (__    
+| |   | || |   | || (\ \) ||  __)   
+| |   ) || |   | || | \   || (      
+| (__/  )| (___) || )  \  || (____/\
+(______/ (_______)|/    )_)(_______/
+                                    
+	THANK YOU FOR USING MY TOOL..
+	
+	
+	""")
+else:
+  print("\n\n\033[1;31mYOU HAVE INTERED THE WRONG OPTION\n\n\033[1;36mTO RUN AGAIN TYPE : \033[1;32mpython Complete_command.py \n\n")
+  os.system("exit")
+
